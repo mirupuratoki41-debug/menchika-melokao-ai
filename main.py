54979b130d468cdaa897fb65650460a02cff387e
Toki <mirupuratoki41@gmail.com>
2026-09-27 00:59:15 +0900

    Remove unused requests import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
-import requests
 from typing import List, Optional
 
 app = FastAPI(title="Men-Chika Mero-Face AI Diagnosis API")
